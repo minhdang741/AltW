535ef53b8746cc0eec7c9033a41a96a9e3f1bff9
minhdang741 <73865688+minhdang741@users.noreply.github.com>
2023-06-19 18:17:45 +0700

    update adblock for chrome

---
+++ Adblock_Chrome/Adblock/scripts/main.js
@@ -0,0 +1,30 @@
+/** 
+ * Click on extension to toggle on/off and get original tab's id
+ * */
+// var originalTab = 0;
+// var isEnabled = false;
+// chrome.action.onClicked.addListener((currentTab) => {
+//     isEnabled = !isEnabled;
+//     console.log('currentTab: ', currentTab);
+//     originalTab = currentTab.id;
+//     console.log('originalTab: ', originalTab);
+//     console.log('enabled: ', isEnabled);
+//     window.open("www.google.com","_blank");
+// });
+
+
+/** 
+ * Close any new tab that not having pending url (ad)
+ * */
+chrome.tabs.onCreated.addListener(async (newTab) => {
+    console.log('newTab: ', newTab);
+    console.log('pending Url: ', newTab.pendingUrl);
+    if(!newTab.pendingUrl) {
+        chrome.tabs.remove(newTab.id);
+    }
+});
+
+
+// chrome.tabs.onActivated.addListener((activeTab) => {
+//     console.log('activeTab: ', activeTab);
+// });
